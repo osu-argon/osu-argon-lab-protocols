5e4a119e7b3ea11c0671adc1df37b26371efe5dd
Rupert Minnett <rminnett@Gmail.com>
2021-04-06 16:08:36 -0400

    Restructure and deploy to S3 action

---
+++ content/conf.py
@@ -0,0 +1,69 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'osu-argon-lab-protocols'
+copyright = '2021, Daniel Heaton'
+author = 'Daniel Heaton'
+
+# The full version, including alpha/beta/rc tags
+release = '1.0.0'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "recommonmark"
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '_old']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+import sphinx_pangeo_theme
+html_theme = "pangeo"
+html_theme_path = [sphinx_pangeo_theme.get_html_theme_path()]
+html_sidebars = {
+    'index': [],
+    '**': [
+        'localtoc.html',
+        'srclinks.html'
+    ]
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
---
+++ content/index.rst
@@ -0,0 +1,35 @@
+.. osu-argon-lab-protocols documentation master file, created by
+   sphinx-quickstart on Thu Mar 25 11:58:58 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to the OSU Argon Geochronology Lab Protocols!
+=====================================================
+
+.. toctree::
+   :caption: Introduction:
+   :glob:
+   :titlesonly:
+
+   introduction/**
+
+.. toctree::
+   :caption: Recipes:
+   :glob:
+   :maxdepth: 1
+   
+   recipes/**
+
+.. toctree::
+   :caption: Methods:
+   :glob:
+   :maxdepth: 1
+
+   methods/**
+
+.. toctree::
+   :caption: Tasks:
+   :glob:
+   :maxdepth: 1
+
+   tasks/**
